b4e0bdbfa92fdcfd83e4de5a1e777dadfc14e31a
Sandra Israel <israel.sandra@atslng.com>
2024-10-23 17:42:37 +0100

    a client page component

---
+++ src/Pages/Client.jsx
@@ -0,0 +1,62 @@
+import { BACKEND_URL } from "@/constants"
+import { useQuery } from "@tanstack/react-query"
+
+
+
+
+
+export default function ClientsPage(){
+
+    const clients = {
+        name: 'sandra',
+        website: 'https://www.com'
+    }
+
+    const { data, error} = useQuery({
+        queryKey: ['clientsData'],
+        queryFn: async () => {
+            const res = await fetch(`${BACKEND_URL}/clients`, {
+                method: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${token}`, // Pass JWT via Authorization header
+                },  
+            })
+            if (!res.ok) {
+                throw new Error(error.message);
+            }
+
+            return res.json(); 
+        } 
+        
+    })
+    console.log(data)       
+    
+
+    return (
+
+        <table>
+            <thead>
+                <tr>
+                    <th>Name:</th>
+                    <th>Website: {data}</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <td>{clients.name}</td>
+                    <td><a href={clients.website}>{clients.website}</a></td>
+                </tr>
+            </tbody>
+            
+            {/* {data.map(client => (
+                <tr key={client.id}>
+                    <td>{client.name}</td>
+                    <td><a href={client.website}>{client.website}</a></td>
+                </tr>
+            ))} */}
+        </table>
+    
+  )
+
+}
