03bddddd1ae94830949dff0da886d3092c63f8e0
Sandra Israel <israel.sandra@atslng.com>
2024-10-24 18:06:35 +0100

    added state variable that handles sidebar visibility

--- src/Pages/UserAreaLayout.jsx
+++ src/Pages/UserAreaLayout.jsx
@@ -10,6 +10,7 @@ import { useNavigate } from "react-router-dom";
 import { RequireAuth } from "./Auth";
 import { AUTH_DATA_KEY, BACKEND_URL } from "@/constants"
 import { getDataObject } from "@/functions"
+import { useState } from "react"
 
 export const description = 
     "A products dashboard with a sidebar navigation and a main content area. The dashboard has a header with a search input and a user menu. The sidebar has a logo, navigation links, and a card with a call to action. The main content area shows an empty state with a call to action."
@@ -19,6 +20,17 @@ export default function UserAreaLayout() {
     const navigate = useNavigate();
     // const auth = useContext(AuthContext)
 
+    const [openSidebar, setOpenSidebar] = useState(false);
+
+    const handleToggle = () => {
+        setOpenSidebar(!openSidebar);
+    };
+
+    const handleClose = () => {
+        setOpenSidebar(false);
+    };
+
+
     async function logOut() {
         try{
             const token = getDataObject(AUTH_DATA_KEY)?.accessToken
@@ -26,7 +38,7 @@ export default function UserAreaLayout() {
                 method: 'POST',
                 headers: {
                     'Authorization': `Bearer ${token}`
-                },  
+                },    
             })
 
             if (!res.ok) return alert('Logout failed! Try again.')
@@ -70,47 +82,17 @@ export default function UserAreaLayout() {
                                 <Users className="h-4 w-4" />
                                 Clients   
                             </Link>
-                            {/*<Link href="#" className="flex items-center gap-3 rounded-lg bg-muted px-3 py-2 text-primary transition-all hover:text-primary">
-                                <Package className="h-4 w-4" />
-                                    Products{" "}
-                            </Link>
-                            <Link href="#" className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary">
-                                <Users className="h-4 w-4" />
-                                Customers
-                            </Link>
-                            <Link href="#" className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary">
-                                <LineChart className="h-4 w-4" />
-                                Analytics
-                            </Link> */}
                         </nav>
                     </div>
-
-                    {/* containing the card below in the sidebar */}
-                    {/* <div className="mt-auto p-4">
-                        <Card x-chunk="dashboard-02-chunk-0">
-                            <CardHeader className="p-2 pt-0 md:p-4">
-                                <CardTitle>Upgrade to Pro</CardTitle>
-                                <CardDescription>
-                                    Unlock all features and get unlimited access to our support
-                                    team.
-                                </CardDescription>
-                            </CardHeader>
-                            <CardContent className="p-2 pt-0 md:p-4 md:pt-0">
-                                <Button size="sm" className="w-full">
-                                    Upgrade
-                                </Button>
-                            </CardContent>
-                        </Card>
-                    </div> */}
                 </div>
             </div>
 
             {/* containing responsive sidebar */}
             <div className="flex flex-col">
                 <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
-                    <Sheet>
+                    <Sheet open={openSidebar}>
                         <SheetTrigger asChild>
-                            <Button variant="outline" size="icon" className="shrink-0 md:hidden">
+                            <Button variant="outline" size="icon" className="shrink-0 md:hidden" onClick={handleToggle}>
                                 <Menu className="h-5 w-5 text-black" />
                                 <span className="sr-only">Toggle navigation menu</span>
                             </Button>
@@ -122,50 +104,16 @@ export default function UserAreaLayout() {
                                     <Package2 className="h-6 w-6" />
                                     <span className="sr-only text-black">{auth.username}</span>
                                 </Link> */}
-                                <Link to={'/dashboard'}className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground">
+                                <Link to={'/dashboard'}className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground" onClick={handleClose}>
                                     <Home className="h-5 w-5" />
                                     Dashboard
                                 </Link>
-                                <Link to={'/clients'} className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary">
+                                <Link to={'/clients'} className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary" onClick={handleClose}>
                                     <Users className="h-4 w-4" />
                                     Clients   
                                 </Link>
-                                {/* <Link href="#" className="mx-[-0.65rem] flex items-center gap-4 rounded-xl bg-muted px-3 py-2 text-foreground hover:text-foreground">
-                                    <ShoppingCart className="h-5 w-5" />
-                                        Orders
-                                    <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
-                                        6
-                                    </Badge>
-                                </Link>
-                                <Link href="#" className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground">
-                                    <Package className="h-5 w-5" />
-                                        Products
-                                </Link>
-                                <Link href="#" className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground">
-                                    <Users className="h-5 w-5" />
-                                        Customers
-                                </Link>
-                                <Link href="#" className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground">
-                                    <LineChart className="h-5 w-5" />
-                                        Analytics
-                                </Link> */}
+                               
                             </nav>
-                            {/* <div className="mt-auto">
-                                <Card>
-                                    <CardHeader>
-                                        <CardTitle>Upgrade to Pro</CardTitle>
-                                        <CardDescription>
-                                            Unlock all features and get unlimited access to our
-                                            support team.
-                                        </CardDescription>
-                                    </CardHeader>
-                                    <CardContent>
-                                        <Button size="sm" className="w-full">
-                                            Upgrade
-                                        </Button>
-                                    </CardContent>
-                                </Card>
-                            </div> */}
                         </SheetContent>
                     </Sheet>
                     <div className="w-full flex-1">
