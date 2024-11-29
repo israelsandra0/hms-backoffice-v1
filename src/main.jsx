af71be2d15ee02776c6466beb0196065113ec3da
israel sandra <israelsandra2000@gmail.com>
2024-11-29 14:47:34 +0100

    add view hotel pages route

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import UserAreaLayout from './Pages/UserAreaLayout';
 import Dashboard from './Pages/Dashboard';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import HotelsPage from './Pages/hotels/Index';
-import Add from './Pages/hotels/Add';
+import Add from './pages/hotels/Add';
 import Account from './Pages/Account';
 import Reports from './Pages/Reports';
 import Communication from './Pages/Communication';
@@ -16,6 +16,11 @@ import Support from './Pages/Support';
 import Settings from './Pages/Settings';
 import Users from './Pages/Users';
 import HotelsOverview from './pages/hotels/viewHotels/Overview';
+import HotelPageUsers from './pages/hotels/viewHotels/Users';
+import Locations from './pages/hotels/viewHotels/Locations';
+import Rooms from './pages/hotels/viewHotels/RoomType';
+import SubscriptionHistory from './pages/hotels/viewHotels/Subscription';
+import PageSettings from './pages/hotels/viewHotels/Settings';
 
 
 
@@ -28,6 +33,11 @@ const router = createBrowserRouter(
         <Route path='/dashboard' element={<Dashboard />} />
         <Route path='/hotels' element={<HotelsPage />} />
         <Route path='/hotels/view/overview' element={<HotelsOverview />} />
+        <Route path='/hotels/view/users' element={<HotelPageUsers />} />
+        <Route path='/hotels/view/locations' element={<Locations />} />
+        <Route path='/hotels/view/rooms' element={<Rooms />} />
+        <Route path='/hotels/view/suscription_history' element={<SubscriptionHistory />} />
+        <Route path='/hotels/view/settings' element={<PageSettings />} />
         <Route path='/hotels/add' element={<Add />} />
         <Route path='/users' element={<Users />} />
         <Route path='/accounts' element={<Account />} />
