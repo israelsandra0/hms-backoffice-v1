066cf151495844d11c7fbbf8b5c53718c6f444b0
israel sandra <israel.sandra@atslng.com>
2025-01-13 15:42:59 +0100

    work on the search input

--- src/components/ui/intlphone-field.jsx
+++ src/components/ui/intlphone-field.jsx
@@ -1,7 +1,7 @@
 import { ChevronDown, Search } from "lucide-react";
 import { Input } from "./input";
 import { forwardRef, useMemo, useState } from "react";
-import {  usePhoneInput, defaultCountries, FlagImage } from "react-international-phone";
+import { usePhoneInput, defaultCountries, FlagImage } from "react-international-phone";
 
 
 const IntlPhoneField = forwardRef(({ ...props }, ref) => {
@@ -30,13 +30,13 @@ const IntlPhoneField = forwardRef(({ ...props }, ref) => {
         setVisible(!visible)
     }
 
-   
+
     const filteredCountries = useMemo(() => {
         return defaultCountries.filter(([fullName]) => {
             return fullName.toLowerCase().includes(searchFilter.toLowerCase());
         });
-    }, [defaultCountries, searchFilter]) 
-        
+    }, [defaultCountries, searchFilter])
+
 
 
     return (
@@ -44,44 +44,45 @@ const IntlPhoneField = forwardRef(({ ...props }, ref) => {
             <div className="w-full relative">
                 <div className="bg-grey flex p-1 border border-neutral-200 rounded-[5px]">
                     <div className="bg-white text-[#333333] px-2 flex rounded-[5px] gap-2 w-[150px] justify-start items-center" onClick={toggle}>
-                        <FlagImage iso2={country.iso2}  className="w-6" />
-                        <span>+{country.dialCode}</span>  
+                        <FlagImage iso2={country.iso2} className="w-6" />
+                        <span>+{country.dialCode}</span>
                         <ChevronDown className="w-6 h-6" />
                     </div>
-                    <Input className="border-none" {...props} ref={ref} onChange={handlePhoneValueChange} value={inputValue}/>
+                    <Input className="border-none" {...props} ref={ref} onChange={handlePhoneValueChange} value={inputValue} />
                 </div>
 
-                <div className={`px-4 ${visible ? '' : 'hidden'} bg-white shadow-lg  h-[220px] overflow-y-scroll rounded-[5px] absolute w-full border border-neutral-200`}>
+                <div className={`px-4 ${visible ? '' : 'hidden'} bg-white shadow-lg  h-[220px] z-20 overflow-y-scroll rounded-[5px] absolute w-full border border-neutral-200`}  >
                     
-                    <div className=" sticky top-0 bg-white shadow-[-1px_4px_2px_-2px_rgba(0,0,0,0.1)] pl-4 py-4 ">
 
-                        <Search className="text-gray-300 w-4 absolute mt-[10px] ml-4" />
-                        <input
+                    <div className=" sticky top-0 bg-white shadow-[-1px_8px_10px_-2px_rgba(0,0,0,0.1)] py-4 ">
+
+                        <Search className="text-gray-400 w-4 absolute mt-[10px] ml-4" />
+                        <Input
                             type="text"
                             placeholder="Search"
                             value={searchFilter}
                             onChange={(e) => setSearchFilter(e.target.value)}
-                            className="border border-b-gray-300 pl-9 rounded px-4 py-2 w-[300px] outline-none"
+                            className="border border-b-gray-300 bg-transparent rounded px-4 pl-9 outline-none"
                         />
                     </div>
-                    {filteredCountries.map(([fullName, shortName, dialCode]) =>(
+                    {filteredCountries.map(([fullName, shortName, dialCode]) => (
 
-                        <div 
-                            key={shortName} 
-                            className={`flex justify-between cursor-pointer p-2  rounded-[5px] ${shortName == country.iso2 ? 'bg-blue-100' : 'hover:bg-gray-100'}`}  
+                        <div
+                            key={shortName}
+                            className={`flex justify-between cursor-pointer p-2  rounded-[5px] ${shortName == country.iso2 ? 'bg-blue-100' : 'hover:bg-gray-100'}`}
                             onClick={() => {
-                                setCountry(shortName) 
+                                setCountry(shortName)
                                 setSearchFilter("")
-                                toggle() 
+                                toggle()
                             }}
                         >
                             <div className="flex  gap-2">
-                                <FlagImage iso2={shortName} className="w-6" /> {fullName} 
+                                <FlagImage iso2={shortName} className="w-6" /> {fullName}
                             </div>
-                            
+
                             <div className="text-gray-500">+{dialCode}</div>
-                            
-                        </div>                      
+
+                        </div>
 
                     ))}
                 </div>
