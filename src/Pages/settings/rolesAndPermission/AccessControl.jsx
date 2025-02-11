fd5346894ab50777899e0d946c934b50de5afd5b
israel sandra <israel.sandra@atslng.com>
2025-02-11 17:17:15 +0100

    add content for listing of roles and description

---
+++ src/Pages/settings/rolesAndPermission/AccessControl.jsx
@@ -0,0 +1,207 @@
+import Pagination from "@/components/Pagination";
+import UserAreaHeader from "@/components/UserAreaHeader";
+import { Breadcrumb, BreadcrumbItem, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
+import { Button } from "@/components/ui/button";
+import Spinner from "@/components/ui/spinner";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { get } from "@/functions";
+import { useQuery } from "@tanstack/react-query";
+import { flexRender, getCoreRowModel, getPaginationRowModel, useReactTable } from "@tanstack/react-table";
+import { MoreVertical, Search, Shield } from "lucide-react";
+import { useEffect, useMemo, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import AddRole from "./AddRole";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import EditRole from "./EditRole";
+
+export default function AccessControl() {
+
+    const navigate = useNavigate()
+    const [roles, setRoles] = useState([])
+    const [searchFilter, setSearchFilter] = useState("");
+    const [addRoleBox, setAddRoleBox] = useState(false);
+    const [editRoleBox, setEditRoleBox] = useState(false);
+    const [editId, setEditId] = useState(null)
+
+    const [pageIndex, setPageIndex] = useState(0);
+    const pageSize = 10
+
+
+    const columns = useMemo(() => [
+        {
+            header: "Name",
+            accessorKey: "name",
+        },
+        {
+            header: "Description",
+            accessorKey: "description",
+        },
+        {
+            id: "actions",
+            enableHiding: false,
+            cell: ({ row }) => {
+                return (
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <MoreVertical className="cursor-pointer" />
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent className="w-56 cursor-pointer">
+                            <DropdownMenuItem
+                                onClick={() => { setEditId(row.original); setEditRoleBox(true)}}
+                            >
+                                <span>Edit</span>
+                            </DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                );
+            },
+        },
+    ], []);
+
+    const filteredRole = useMemo(() => {
+        if (!searchFilter) return roles || [];
+
+        const lowerCaseFilter = searchFilter.toLowerCase();
+
+        return (roles || []).filter(role =>
+            role.name.toLowerCase().includes(lowerCaseFilter)
+        );
+    }, [roles, searchFilter]);
+
+
+    const table = useReactTable({
+        data: filteredRole,
+        columns,
+        state: {
+            pagination: {
+                pageIndex,
+                pageSize,
+            },
+        },
+        onPaginationChange: ({ pageIndex }) => {
+            setPageIndex(pageIndex ?? 0);
+        },
+        getPaginationRowModel: getPaginationRowModel(),
+        getCoreRowModel: getCoreRowModel(),
+    });
+
+
+    const { isLoading, refetch: fetchRoles } = useQuery({
+        queryKey: ["roles"],
+        queryFn: async () => {
+            const res = await get(`/roles`);
+            if (!res.ok) {
+                throw new Error("Failed to fetch data");
+            }
+            const response = await res.json();
+            setRoles(response.data)
+            return response.data;
+        },
+        enabled: false
+    });
+
+    useEffect(() => {
+        fetchRoles()
+    }, [])
+
+    const closeAddRoleBox = () => {
+        setAddRoleBox(false)
+        fetchRoles()
+    }
+    const handleEditClose = () => {
+        setEditRoleBox(false)
+        fetchRoles()
+    }
+
+
+    const breadcrumb = (
+        <Breadcrumb>
+            <BreadcrumbList>
+                <BreadcrumbItem>
+                    <Link onClick={() => navigate('/hotels')}>Hotels</Link>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator />
+                <BreadcrumbItem>
+                    Setting
+                </BreadcrumbItem>
+                <BreadcrumbSeparator className='mt-1' />
+                <BreadcrumbItem>
+                    <BreadcrumbPage>Access Control</BreadcrumbPage>
+                </BreadcrumbItem>
+            </BreadcrumbList>
+        </Breadcrumb>
+    );
+
+
+    return (
+        <div>
+            <UserAreaHeader pages={breadcrumb} />
+
+            {isLoading && (
+                <div className="text-center flex items-center justify-center mx-auto my-5">
+                    <Spinner className="me-3 text-gray-300 h-16 w-16" />
+                </div>
+            )}
+
+            {!isLoading && !roles?.length && (
+                <div className='mx-auto items-center mt-16 grid place-items-center text-center'>
+                    <div className="bg-grey w-[170px] grid place-items-center  h-[170px] rounded-[50%]">
+                        <Shield className='w-[100px] h-[100px] text-primary' />
+                    </div>
+                    <h1 className="text-[1.5rem] my-6  font-bold">No Data Found!</h1>
+                    <Button variant='primary' onClick={() => ( setAddRoleBox(true))}>Add</Button>
+                </div>
+            )}
+            {!!roles?.length && (
+                <div>
+                    <div className="flex justify-between mx-6 my-8 ">
+                        <div className="mb-4">
+                            <Search className="text-gray-300 w-4 absolute mt-[10px] ml-4" />
+                            <input
+                                type="text"
+                                placeholder="Search"
+                                value={searchFilter}
+                                onChange={(e) => setSearchFilter(e.target.value)}
+                                className="border border-b-gray-300 pl-9 rounded px-4 py-2 w-[300px] outline-none"
+                            />
+                        </div>
+                        <Button variant='primary' onClick={() => ( setAddRoleBox(true))}>Add</Button>
+                    </div>
+                    <div className="content w-[95%] my-6 mx-auto rounded-[8px] border border-gray-200 overflow-hidden">
+                        {table.getPageCount() > 0 && (
+                            <Table>
+                                <TableHeader className="bg-lightPrimary">
+                                    <TableRow>
+                                        <TableHead>Name</TableHead>
+                                        <TableHead>Description</TableHead>
+                                        <TableHead></TableHead>
+                                    </TableRow>
+                                </TableHeader>
+                                <TableBody>
+                                    {table.getRowModel().rows.map((row) => (
+                                        <TableRow key={row.id} className="hover:bg-grey">
+                                            {row.getVisibleCells().map((cell) => (
+                                                <TableCell key={row.id}>
+                                                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                                </TableCell>
+                                            ))}
+                                        </TableRow>
+                                    ))}
+                                </TableBody>
+                            </Table>
+                        )}
+
+                        <Pagination table={table} pageIndex={pageIndex} setPageIndex={setPageIndex} />
+
+                    </div>
+                </div>
+            )}
+
+            {!!addRoleBox && (
+                <AddRole closeFn={closeAddRoleBox} />
+            )}
+
+            {!!editRoleBox && <EditRole closeFn={handleEditClose} editId={editId} />}
+        </div>
+    )
+}
