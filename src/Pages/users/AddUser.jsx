bb8f8257a6dc26f0790156febe47ba388cfd075b
israel sandra <israel.sandra@atslng.com>
2025-02-13 15:39:52 +0100

    add role data

--- src/Pages/users/AddUser.jsx
+++ src/Pages/users/AddUser.jsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import { Controller, useForm } from "react-hook-form";
@@ -15,8 +15,7 @@ import IntlPhoneField from "@/components/ui/intlphone-field";
 import PasswordField from "@/components/ui/password-field";
 import { useToast } from "@/hooks/use-toast";
 
-
-export default function AddUsers({ closeFn }) {
+export default function AddUsers({ closeFn, roleData }) {
     // Yup schema
     const yupBuild = yup.object({
         firstName: yup.string().required("First Name is required").max(25),
@@ -55,32 +54,29 @@ export default function AddUsers({ closeFn }) {
             roleId: "",
             isAdmin: false,
             password: "",
-            autoGeneratePassword: false
+            autoGeneratePassword: false,
         },
         resolver: yupResolver(yupBuild),
     });
 
     const [errorMessage, setErrorMessage] = useState("");
     const [disabledButton, setDisabledButton] = useState(false);
-    const { toast } = useToast()
-    const [autoGeneratePassword, setGeneratePassword] = useState(false)
-    const [isAdmin, setIsAdmin] = useState(false)
-
+    const { toast } = useToast();
+    const [autoGeneratePassword, setGeneratePassword] = useState(false);
+    const [isAdmin, setIsAdmin] = useState(false);
 
     const handleCheckboxChange = (e) => {
         setGeneratePassword(e.target.checked);
-        setValue('autoGeneratePassword', e.target.checked)
+        setValue("autoGeneratePassword", e.target.checked);
     };
 
     const handleAdminChange = (e) => {
         setIsAdmin(e.target.checked);
-        setValue('isAdmin', e.target.checked)
+        setValue("isAdmin", e.target.checked);
     };
-
 
     const { mutate } = useMutation({
         mutationFn: async () => {
-
             const userInput = getValues();
             setErrorMessage("");
             setDisabledButton(true);
@@ -94,7 +90,7 @@ export default function AddUsers({ closeFn }) {
                     roleId: userInput.roleId,
                     isAdmin: userInput.isAdmin,
                     password: userInput.password,
-                    autoGeneratePassword: userInput.autoGeneratePassword
+                    autoGeneratePassword: userInput.autoGeneratePassword,
                 };
                 const res = await post(`/users/store`, userData);
 
@@ -102,10 +98,9 @@ export default function AddUsers({ closeFn }) {
                     toast({
                         success: true,
                         duration: 5000,
-                        title: 'User added successfully!'
+                        title: "User added successfully!",
                     });
-                    closeFn()
-
+                    closeFn();
                 } else if (res.status.toString().startsWith(4)) {
                     setDisabledButton(false);
                     setErrorMessage(
@@ -132,21 +127,22 @@ export default function AddUsers({ closeFn }) {
                 }
 
                 const responseData = await res.json();
-                closeFn()
+                closeFn();
                 setDisabledButton(true);
-
 
                 return responseData;
             } catch (error) {
                 console.log(error);
             }
         },
-    })
-
+    });
 
     return (
         <div className="fixed inset-x-0 inset-y-0 bg-black/50 h-screen flex justify-center items-center">
-            <CardContent style={{ borderRadius: '16px' }} className='fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 bg-white p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg dark:border-neutral-800 dark:bg-neutral-950'>
+            <CardContent
+                style={{ borderRadius: "16px" }}
+                className="fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 bg-white p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg dark:border-neutral-800 dark:bg-neutral-950"
+            >
                 {!!errorMessage?.length && (
                     <Alert className="alert text-red-900 border-0 h-full  bg-[#fee] mb-4">
                         <AlertDescription>{errorMessage}</AlertDescription>
@@ -155,36 +151,40 @@ export default function AddUsers({ closeFn }) {
 
                 <div className="flex mt-4">
                     <Link>
-                        <X className="ring-2 p-1 ring-[#F2F2F5] rounded-full text-gray-400" onClick={closeFn} />
+                        <X
+                            className="ring-2 p-1 ring-[#F2F2F5] rounded-full text-gray-400"
+                            onClick={closeFn}
+                        />
                     </Link>
                     <h1 className="text-[1.3rem] font-bold mx-auto">Add New Users</h1>
                 </div>
 
-                <form
-                    className="hotelForm text-left"
-                    onSubmit={handleSubmit(mutate)}
-                >
+                <form className="hotelForm text-left" onSubmit={handleSubmit(mutate)}>
                     {/* {JSON.stringify(errors)} */}
                     <div className="mt-4">
-
                         <div className="mb-2">
                             <Label htmlFor="firstName">First Name</Label>
                             <br />
-                            <Input {...register("firstName")} id="firstName" maxLength='25' />
+                            <Input {...register("firstName")} id="firstName" maxLength="25" />
                             <p>{errors.firstName?.message}</p>
                         </div>
 
                         <div className="mb-2">
                             <Label htmlFor="lastName">Last Name</Label>
                             <br />
-                            <Input {...register("lastName")} id="lastName" maxLength='25' />
+                            <Input {...register("lastName")} id="lastName" maxLength="25" />
                             <p>{errors.lastName?.message}</p>
                         </div>
 
                         <div className="mb-2">
                             <Label htmlFor="email">Email</Label>
                             <br />
-                            <Input {...register("email")} type="email" id="email" maxLength='50' />
+                            <Input
+                                {...register("email")}
+                                type="email"
+                                id="email"
+                                maxLength="50"
+                            />
                             <p>{errors.email?.message}</p>
                         </div>
 
@@ -194,36 +194,35 @@ export default function AddUsers({ closeFn }) {
                             <Controller
                                 name="phone"
                                 control={control}
-                                render={({ field }) => (
-                                    <IntlPhoneField {...field} id="phone" />
-                                )}
+                                render={({ field }) => <IntlPhoneField {...field} id="phone" />}
                             />
                             <p>{errors.phone?.message}</p>
                         </div>
 
-
                         {!isAdmin && (
-
                             <div className="mb-2">
                                 <Label htmlFor="roleId">RoleId</Label>
                                 <br />
-                                <Input {...register("roleId")} id="roleId" maxLength='25' />
+
+                                <select {...register("roleId")} className="border w-16 outline-none">
+                                    {roleData.map((data) => (
+                                        
+                                        <option value={data.id}>{data.id}</option>
+                                    ))}
+                                </select>
                                 <p>{errors.roleId?.message}</p>
                             </div>
                         )}
-                        <div className="mt-2 flex items-center">
-                            <input type="checkbox"
+                        <div className="mt-2 flex items-center my-2">
+                            <input
+                                type="checkbox"
                                 checked={isAdmin}
                                 onChange={handleAdminChange}
                             />
-                            <h2 className="ml-2 text-[12px]">
-                                isAdmin?
-                            </h2>
+                            <h2 className="ml-2 text-[12px]">isAdmin?</h2>
                         </div>
 
-
                         {!autoGeneratePassword && (
-
                             <div>
                                 <Label htmlFor="password">Password</Label>
                                 <Controller
@@ -232,19 +231,16 @@ export default function AddUsers({ closeFn }) {
                                     render={({ field }) => <PasswordField {...field} />}
                                 />
 
-
                                 <p className="text-red-700">{errors.password?.message}</p>
-
                             </div>
                         )}
                         <div className="mt-2 flex items-center">
-                            <input type="checkbox"
+                            <input
+                                type="checkbox"
                                 checked={autoGeneratePassword}
                                 onChange={handleCheckboxChange}
                             />
-                            <h2 className="ml-2 text-[12px]">
-                                auto-generate password?
-                            </h2>
+                            <h2 className="ml-2 text-[12px]">auto-generate password?</h2>
                         </div>
                     </div>
 
@@ -259,7 +255,6 @@ export default function AddUsers({ closeFn }) {
                     </Button>
                 </form>
             </CardContent>
-
         </div>
     );
 }
