2553b76565f7483d65b732e217c53cec8072d9a5
israel sandra <israel.sandra@atslng.com>
2025-02-17 11:55:31 +0100

    increase input length and role name

--- src/Pages/users/AddUser.jsx
+++ src/Pages/users/AddUser.jsx
@@ -201,10 +201,11 @@ export default function AddUsers({ closeFn, roleData }) {
 
                         {!isAdmin && (
                             <div>
-                                <Label htmlFor="roleId">RoleId</Label>
+                                <Label htmlFor="role">Role</Label>
                                 <br />
 
-                                <select {...register("roleId")} className="flex h-10 w-1/2 rounded border border-neutral-200 bg-[#F2F2F5] px-3 autofill:bg-[#F2F2F5] py-2 text-sm  file:border-0 file:bg-[#F2F2F5] file:text-sm file:font-medium file:text-neutral-950 placeholder:text-[#BBBAC5] focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 dark:border-neutral-800 dark:bg-[#F2F2F5] dark:ring-offset-neutral-950 outline-none text-black dark:file:text-neutral-50 dark:placeholder:text-neutral-400 ">
+                                <select name='Select role' {...register("roleId")} className="flex h-10 w-full rounded border border-neutral-200 bg-[#F2F2F5] px-3 autofill:bg-[#F2F2F5] py-2 text-sm  file:border-0 file:bg-[#F2F2F5] file:text-sm file:font-medium file:text-neutral-950 placeholder:text-[#BBBAC5] focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 dark:border-neutral-800 dark:bg-[#F2F2F5] dark:ring-offset-neutral-950 outline-none text-black dark:file:text-neutral-50 dark:placeholder:text-neutral-400 ">
+                                    <option selected disabled>Select role</option>
                                     {roleData.map((data) => (
 
                                         <option value={data.id}>{data.name}</option>
