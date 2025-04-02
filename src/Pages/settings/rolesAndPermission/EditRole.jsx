d4cc8db62f2e4572b1a9804310ac6eedb252cea2
israel sandra <israel.sandra@atslng.com>
2025-04-02 18:09:15 +0100

    convert modal to a page

--- src/Pages/settings/rolesAndPermission/EditRole.jsx
+++ src/Pages/settings/rolesAndPermission/EditRole.jsx
@@ -2,24 +2,29 @@ import { Button } from "@/components/ui/button";
 import { CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import Spinner from "@/components/ui/spinner";
+import UserAreaHeader from "@/components/UserAreaHeader";
 import { get, put } from "@/functions";
 import { useToast } from "@/hooks/use-toast";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { Label } from "@radix-ui/react-label";
 import { useMutation, useQuery } from "@tanstack/react-query";
-import { X } from "lucide-react";
 import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import * as yup from "yup";
+import { Breadcrumb, BreadcrumbItem, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
 
 
 
-export default function EditRole({ closeFn, editId }) {
 
+export default function EditRole() {
+
+    const location = useLocation();
+    const { editId } = location.state || {};  
     const [errorMessage, setErrorMessage] = useState("");
     const [permissionCategories, setPermissionCategories] = useState([]);
     const [selectedPermissions, setSelectedPermissions] = useState(editId.permissions?.map(permission => permission.id) || []);
+    const navigate = useNavigate()
 
     const yupBuild = yup.object({
         name: yup.string().required("Name is required").max(50),
@@ -46,7 +51,7 @@ export default function EditRole({ closeFn, editId }) {
                     duration: 5000,
                     title: 'Data updated successfully!'
                 });
-                closeFn()
+                navigate('/setting/access_control')
 
             } else if (res.status.toString().startsWith(4)) {
 
@@ -72,7 +77,7 @@ export default function EditRole({ closeFn, editId }) {
     })
 
 
-    const { refetch: permissionForRole } = useQuery({
+    const { refetch: permissionForRole, isFetching } = useQuery({
         queryKey: ["permissionsForRole"],
         queryFn: async () => {
             const res = await get(`/roles/${editId.id}/edit`);
@@ -92,6 +97,7 @@ export default function EditRole({ closeFn, editId }) {
     useEffect(() => {
         permissionForRole();
     }, [permissionForRole]);
+    
 
     // Handle permission change
     const handlePermissionChange = (permissionId) => {
@@ -103,24 +109,37 @@ export default function EditRole({ closeFn, editId }) {
         setValue("permissions", updatedPermissions); // Update form state
     };
 
+    const breadcrumb = (
+            <Breadcrumb>
+                <BreadcrumbList>
+                    <BreadcrumbItem>
+                        Setting
+                    </BreadcrumbItem>
+                    <BreadcrumbSeparator className='mt-1' />
+                    <BreadcrumbItem>
+                        <Link onClick={() => navigate('/setting/access_control')}>Access Control</Link>
+                    </BreadcrumbItem>
+                    <BreadcrumbSeparator className='mt-1' />
+                    <BreadcrumbItem>
+                        <BreadcrumbPage>Edit</BreadcrumbPage>
+                    </BreadcrumbItem>
+                </BreadcrumbList>
+            </Breadcrumb>
+        );
+    
+
 
     return (
 
-        <div className="fixed inset-x-0 inset-y-0 bg-black/50 h-screen flex justify-center items-center">
+        <div>
+            <UserAreaHeader pages={breadcrumb} />
 
-            <CardContent style={{ borderRadius: '16px' }} className='fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 bg-white p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg dark:border-neutral-800 dark:bg-neutral-950'>
+            <CardContent>               
                 {!!errorMessage?.length && (
                     <Alert className="alert text-red-900 border-0 h-full  bg-[#fee]">
                         <AlertDescription>{errorMessage}</AlertDescription>
                     </Alert>
                 )}
-
-                <div className="flex mt-4">
-                    <Link>
-                        <X className="ring-2 p-1 ring-[#F2F2F5] rounded-full text-gray-400" onClick={closeFn} />
-                    </Link>
-                    <h1 className="text-[1.3rem] font-bold mx-auto">Edit Role and Permission</h1>
-                </div>
 
                 <form
                     onSubmit={handleSubmit(mutate)}
@@ -143,28 +162,35 @@ export default function EditRole({ closeFn, editId }) {
                         <div>
                             <b>Select Role Permissions</b>
                             <hr />
-                            <div>
-                                {permissionCategories.map((category) => (
-                                    <div key={category.id}>
-                                        <h3>{category.name}</h3>
-                                        <ul>
-                                            {category.permissions.map((permission) => (
-                                                <li key={permission.id}>
-                                                    <label>
-                                                        <input
-                                                            type="checkbox"
-                                                            value={permission.id}
-                                                            checked={selectedPermissions.includes(permission.id)}
-                                                            onChange={() => handlePermissionChange(permission.id)}
-                                                        />
-                                                        {permission.name} - {permission.category}
-                                                    </label>
-                                                </li>
-                                            ))}
-                                        </ul>
-                                    </div>
-                                ))}
-                            </div>
+                            {isFetching ? (
+                                <div className="text-center mt-4">
+                                    <Spinner className="text-gray-300 h-16 w-16" />
+                                    <p>Loading permissions...</p>
+                                </div>
+                            ) : (
+                                <div>
+                                    {permissionCategories.map((category) => (
+                                        <div key={category.id}>
+                                            <h3>{category.name}</h3>
+                                            <ul>
+                                                {category.permissions.map((permission) => (
+                                                    <li key={permission.id}>
+                                                        <label>
+                                                            <input
+                                                                type="checkbox"
+                                                                value={permission.id}
+                                                                checked={selectedPermissions.includes(permission.id)}
+                                                                onChange={() => handlePermissionChange(permission.id)}
+                                                            />
+                                                            {permission.name} - {permission.category}
+                                                        </label>
+                                                    </li>
+                                                ))}
+                                            </ul>
+                                        </div>
+                                    ))}
+                                </div>
+                            )}
                         </div>
                     </div>
 
