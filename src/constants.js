f071651a17a9546e56da7f77f87a676649fc0709
Israel Sandra <israelsandra2000@gmail.com>
2025-09-09 13:00:27 +0100

    add a database key

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,4 @@
 export const AUTH_DATA_KEY = 'auth';
 export const USER_DATA_KEY = 'user'
 export const BACKEND_URL = 'https://hms-v1.atslng.com'
+export const DATABASE_KEY = '3vXAQue4AvFC4CLyo689mLS6fVAP-8dPukQPCVzKwYfKWfV9RNeEf2mRZzBHSsiJ.eTBwUTJpLXFZSkpydmlsag.CFjxJU_Be2Lkh12fFZfR8SSwiFi6xY5FCSh-0VtHr14';
