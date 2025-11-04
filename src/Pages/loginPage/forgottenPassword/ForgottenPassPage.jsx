ff68529845d59ce900e7f9cb7d179b73702262e7
Israel Sandra <israelsandra2000@gmail.com>
2025-11-04 15:46:50 +0100

    added a forgotten password page

--- src/Pages/loginPage/forgottenPassword/ForgottenPassPage.jsx
+++ src/Pages/loginPage/forgottenPassword/ForgottenPassPage.jsx
@@ -7,13 +7,21 @@ import * as yup from "yup";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Label } from "@/components/ui/label";
+import { useMutation } from "@tanstack/react-query";
+import { post } from "@/functions";
+import { toast, useToast } from "@/hooks/use-toast";
+import VerificationPage from "./OTPVerificationPage";
 
 export default function ForgottenPassPage() {
 
-    const navigate = useNavigate()
-	
+	const navigate = useNavigate()
+	const [showVerification, setShowVerification] = useState(false);
+	const [userEmail, setUserEmail] = useState("")
+	const [dbError, setDbError] = useState("")
+
+
 	const yupBuild = yup.object({
-		email: yup.string().required("user email is required").email()
+		email: yup.string().required("User email is required").email()
 	});
 
 	const {
@@ -26,116 +34,102 @@ export default function ForgottenPassPage() {
 		resolver: yupResolver(yupBuild),
 	});
 
-	const [errorMessage, setErrorMessage] = useState("");
-
-	// //for disabling button after first click
 	const [disabledButton, setDisabledButton] = useState(false);
 
-	// async function getAuthUser() {
-	// 	try {
-	// 		const response = await get('/auth/me');
+	const { mutate } = useMutation({
+		mutationFn: async () => {
+			const userInput = getValues();
+			
+			setDisabledButton(true);
 
-	// 		const { data } = await response.json();
+			try {
+				const loginData = {
+					email: userInput.email
+				};
 
-	// 		setData(USER_DATA_KEY, data);
-	// 	} catch (error) {
-	// 		console.log(error);
-	// 	} finally {
-	// 		console.log("done");
-	// 	}
-	// }
+				const res = await post('/auth/sendotp', loginData);
+				const responseData = await res.json();
 
-	// const { mutate } = useMutation({
-    //     mutationFn: async () => {
-	// 		const userInput = getValues();
+				if (res.ok) {
+					toast({
+						success: true,
+						duration: 5000,
+						title: responseData.message
+					});
 
-	// 		setErrorMessage();
-	// 		setDisabledButton(true);
+					setUserEmail(userInput.email)
+					setShowVerification(true);
 
-	// 		try {
-	// 			const loginData = {
-	// 				email: userInput.email
-	// 			};
+				} else {
+					// toast({
+					// 	error: true,
+					// 	duration: 5000,
+					// 	title: responseData.message
+					// });
 
-	// 			const res = await post('/auth/login', loginData);
+					setDbError(responseData.message)
+				}
 
-	// 			if (res.status.toString().startsWith(4)) {
-	// 				setDisabledButton(false);
-	// 				setErrorMessage("invalid credentials"); 
+				console.log('toast', responseData)
 
-	// 				const responseErrors = await res.json();
+				// setData(AUTH_DATA_KEY, responseData.data);
 
-	// 				if (responseErrors?.message) {
-	// 					setErrorMessage(responseErrors.message); 
-	// 				}
+				// await getAuthUser();
 
-	// 				return null;
-	// 			}
-	// 			if (res.status >= 500) {
-	// 				setDisabledButton(false);
-	// 				setErrorMessage("An error occurred, please try again");
-	// 				return null;
-	// 			}
-	// 			const responseData = await res.json();
+				// setTimeout(() => (window.location.href = "/dashboard"), 100);
 
-	// 			setData(AUTH_DATA_KEY, responseData.data);
-
-	// 			await getAuthUser();
-
-	// 			setTimeout(() => (window.location.href = "/dashboard"), 100);
-
-	// 			setDisabledButton(true);
-	// 			return responseData;
-	// 		} catch (error) {
-	// 			console.log(error);
-	// 		}
-	// 	},
-	// });
+				setDisabledButton(false);
+				return responseData;
+			} catch (error) {
+				console.log(error);
+			}
+		},
+	});
 
 	return (
 		<>
-			<div className="grid place-items-center gap-12 mt-8">
-				{/* {!!errorMessage?.length && (
-					<Alert className="alert text-red-900 border-0 h-full w-[320px]  bg-[#fee]">
-						<AlertDescription>{errorMessage}</AlertDescription>
-					</Alert>
-				)} */}
-
-				<Card className="w-2/5 static rounded-[15px] p-4 mt-8">
-					<CardHeader>
-						<CardTitle className="loginText text-center text-[1.8rem]">
-							Forgot your password?
-						</CardTitle>
-						<p className="text-center text-[1rem]">Enter your email address to receive password reset instructions.</p>
-					</CardHeader>
-					<CardContent>
-						<form>
-							<div className="flex flex-col space-y-1.5 mt-8">
-								
-								<Label htmlFor="email">Email</Label>
-								<Input
-									maxLength='50'
-									{...register("email")}
-									type="email"
-									placeholder="info@atslng.com"
-									id="email"
-								/>
-								<p className="text-red-700">{errors.username?.message}</p>
-							</div>
-							<br />
-							<Button
-								variant="primary"
-                                onClick={() => navigate("/forgotten_password/verification")}
-								disabled={!!disabledButton}
-								type="submit"
-								className=" w-full p-[16px] rounded-r-[1.3rem] rounded-l-[1.3rem] text-[16px]"
-							>
-								{disabledButton ? "Submitting..." : "Submit"}
-							</Button>
-						</form>
-					</CardContent>
-				</Card>
-			</div>
+			{showVerification ? (
+				<VerificationPage email={userEmail} />  
+			) : (
+				<div className="grid place-items-center gap-12 mt-8">
+					<Card className="w-2/5 static rounded-[15px] p-4 mt-8">
+						<CardHeader>
+							<CardTitle className="loginText text-center text-[1.8rem]">
+								Forgot your password?
+							</CardTitle>
+							<p className="text-center text-[1rem]">
+								Enter your email address to receive password reset instructions.
+							</p>
+						</CardHeader>
+						<CardContent>
+							<form onSubmit={handleSubmit(mutate)}>
+								<div className="flex flex-col space-y-1.5 mt-8">
+									<Label htmlFor="email">Email</Label>
+									<Input
+										maxLength="50"
+										{...register("email")}
+										type="email"
+										placeholder="info@atslng.com"
+										id="email"
+									/>
+									<p className="text-red-700">{errors.email?.message ? errors.email?.message : dbError}</p>
+									{/* <p className="text-red-700">{dbError}</p> */}
+								</div>
+								<br />
+								<Button
+									variant="primary"
+									disabled={!!disabledButton}
+									type="submit"
+									className="w-full p-[16px] rounded-r-[1.3rem] rounded-l-[1.3rem] text-[16px]"
+								>
+									{disabledButton ? "Submitting..." : "Submit"}
+								</Button>
+							</form>
+						</CardContent>
+					</Card>
+				</div>
+			)}
 		</>
 	);
+
 }
